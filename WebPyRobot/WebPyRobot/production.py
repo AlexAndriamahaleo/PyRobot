73da08c9f0715040cae2948dc5405fea097a0754
Nguyen Thai Son <>
2017-10-03 23:25:07 +0200

    Added production settings

---
+++ WebPyRobot/WebPyRobot/production.py
@@ -0,0 +1,12 @@
+from settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'maturepyrobot',
+        'USER': 'maturepyrobot',
+        'PASSWORD': '0t74WmXW',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
