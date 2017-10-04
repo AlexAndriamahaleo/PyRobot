ef95f63e15a0f832798495600d1e3eef8e186edb
Nguyen Thai Son <>
2017-10-04 08:29:35 +0200

    Nginx, uWsgi, Supervisord conf ...

--- WebPyRobot/WebPyRobot/settings.py
+++ WebPyRobot/WebPyRobot/settings.py
@@ -119,6 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 LOGIN_REDIRECT_URL='backend:login'
 LOGIN_URL='backend:login'
