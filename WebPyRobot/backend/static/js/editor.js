de81745d2870193151467fdf56507c1ff4c7c9c6
AlexAndriamahaleo <alex.andriamahaleo@etu.univ-amu.fr>
2017-10-25 12:53:15 +0200

    enable autocomplete

--- WebPyRobot/backend/static/js/editor.js
+++ WebPyRobot/backend/static/js/editor.js
@@ -1,20 +1,21 @@
 (function ($) {
     $(function () {
-        var cm = CodeMirror.fromTextArea(document.getElementById('ia'), {
+        var editor = CodeMirror.fromTextArea(document.getElementById('ia'), {
             mode: 'python',
-            theme: 'elegant',
+            theme: 'cobalt',
             autoCloseBrackets: true,
             indentUnit: 4,
             lineNumbers: true,
             fixedGutter: true,
             matchBrackets: true,
+            extraKeys: {"Ctrl-Space": "autocomplete"},
             globalVars: true
         });
         $(".save").click(function (e) {
             $("#code").submit();
         });
         $("#code").submit(function () {
-            cm.save();
+            editor.save();
         });
     });
 })(jQuery);
