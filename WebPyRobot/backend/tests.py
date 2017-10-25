6ee8552dd5e4357ffea741fd5aeb120328f0eeb1
Petrole <starcraft2samy@gmail.com>
2017-10-25 12:21:20 +0200

    travis file

--- WebPyRobot/backend/tests.py
+++ WebPyRobot/backend/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import unittest
+from django.test import Client
 
 # Create your tests here.
+
+class SimpleTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get('/')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
