11fa11bbe49a0f8c96bbd59163882101d11feb9b
AlexAndriamahaleo <alex.andriamahaleo@etu.univ-amu.fr>
2018-04-30 16:33:09 +0200

    Update of the editor - contextual autocomplete [starting]

--- WebPyRobot/backend/static/js/editor.js
+++ WebPyRobot/backend/static/js/editor.js
@@ -1,17 +1,49 @@
 (function ($) {
     $(function () {
-        var editor = CodeMirror.fromTextArea(document.getElementById('ia'), {
+            var editor = CodeMirror.fromTextArea(document.getElementById('ia'), {
             mode: 'python',
-            theme: 'cobalt',
+            theme: 'dracula',
+            keyMap: 'sublime',
             autoCloseBrackets: true,
+            lineWrapping: true,
+            foldGutter: true,
+            gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
             indentUnit: 4,
             lineNumbers: true,
             fixedGutter: true,
             matchBrackets: true,
             dragDrop: true,
-            allowDropFileTypes : ["text/x-python","text/plain"],
-            extraKeys: {"Ctrl-Space": "autocomplete", "Option-Space": "autocomplete"},
+            allowDropFileTypes: ["text/x-python", "text/plain"],
+            //extraKeys: {"Ctrl-Space": "autocomplete", "Option-Space": "autocomplete"},
             globalVars: true
+        });
+
+        server = new CodeMirror.TernServer({defs: []});
+        editor.setOption("extraKeys", {
+            "Ctrl-Space": function (cm) {
+                server.complete(cm);
+            },
+            "Ctrl-I": function (cm) {
+                server.showType(cm);
+            },
+            "Ctrl-O": function (cm) {
+                server.showDocs(cm);
+            },
+            "Alt-.": function (cm) {
+                server.jumpToDef(cm);
+            },
+            "Alt-,": function (cm) {
+                server.jumpBack(cm);
+            },
+            "Ctrl-Q": function (cm) {
+                server.rename(cm);
+            },
+            "Ctrl-.": function (cm) {
+                server.selectName(cm);
+            }
+        });
+        editor.on("cursorActivity", function (cm) {
+            server.updateArgHints(cm);
         });
 
         $(".save").click(function (e) {
@@ -21,14 +53,10 @@
         $("#entrer").click(function (e) {
             var file = document.getElementById('myfile');
 
-            //console.log(file.files[0].name);
-
-            if(file.files.length)
-            {
+            if (file.files.length) {
                 var reader = new FileReader();
-                reader.onload = function(e)
-                {
-                    document.getElementById('code_name').setAttribute('value',file.files[0].name);
+                reader.onload = function (e) {
+                    document.getElementById('code_name').setAttribute('value', file.files[0].name);
                     editor.getDoc().setValue(e.target.result);
                 };
 
